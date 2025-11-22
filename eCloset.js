a16e5db2944d5df0bc75c90c555545fadf944d50
Daniela Bordeianu <danabordeianu55@gmail.com>
2025-11-22 14:43:37 -0500

    Added a few imports and the API key

---
+++ eCloset.js
@@ -0,0 +1,4 @@
+import React, { useState, useCallback } from 'react';
+import {Thermometer, CloudRain, Sun, Zap, Snowflake, Search, AlertTriangle, Loader2} from 'lucide-react';
+
+const OPEN_WEATHER_MAP_API_KEY = "f89ab4076e6f473ab6c185156252211";
