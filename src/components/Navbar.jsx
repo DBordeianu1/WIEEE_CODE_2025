5d2fea5e10edfdaa3819545bdc1d93219ca7d250
Daniela Bordeianu <danabordeianu55@gmail.com>
2025-11-22 18:22:51 -0500

    Added the logo of our app into the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import logo from '../assets/eCloset-white-logo.png'
 
 export default function Navbar() {
   return (
     <nav className="navbar">
+      <img src={logo} alt="eCloset Logo" className="navbar-logo"/>
       <h1 className="logo">eCloset</h1>
       <div className="nav-links">
         <Link to="/">Home</Link>
